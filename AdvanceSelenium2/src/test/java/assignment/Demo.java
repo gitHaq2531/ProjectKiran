52fbf23a95053418a0cfd8368ed5ddebaf09c604
kiran2816 <pawarkiran199728@gmail.com>
2026-01-30 17:44:57 +0530

    local reo

--- AdvanceSelenium2/src/test/java/assignment/Demo.java
+++ AdvanceSelenium2/src/test/java/assignment/Demo.java
@@ -13,7 +13,5 @@ public class Demo {
 		System.out.println("What should i update");
 		System.out.println("new one2");
 		System.out.println("ek line add kro");
-		System.out.println("new3");
-		System.out.println("git conflict");
 	}
 }
