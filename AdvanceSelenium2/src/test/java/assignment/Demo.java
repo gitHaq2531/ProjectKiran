00bb1c16283aa37cc6f6a3b73a3f74f250e0ccee
kiran2816 <pawarkiran199728@gmail.com>
2026-01-31 14:31:22 +0530

    com

--- AdvanceSelenium2/src/test/java/assignment/Demo.java
+++ AdvanceSelenium2/src/test/java/assignment/Demo.java
@@ -3,18 +3,6 @@ package assignment;
 public class Demo {
 	//new demo class is created
 	public void changed() {
-		System.out.println("New Method");
-		System.out.println("New line added");
-		System.out.println("second line added");
-		System.out.println("TL updates");
-		System.out.println("tl update2");
-		System.out.println("task complete");
-		System.out.println("new");
-		System.out.println("What should i update");
-		System.out.println("new one2");
-		System.out.println("ek line add kro");
-		System.out.println("new3");
-		System.out.println("new4");
-		System.out.println("new32");
+		
 	}
 }
