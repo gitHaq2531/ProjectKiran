279eb8c17eb682b150413c7a4eca54e1721773db
Jyoti <Jyoti@DESKTOP-NGG3R22>
2026-03-11 11:25:20 +0530

    git demo2 pushing to feature branch

---
+++ AdvanceSelenium2/src/test/java/practiceGit/GitDemo2.java
@@ -0,0 +1,11 @@
+package practiceGit;
+
+public class GitDemo2 {
+
+	public static void main(String[] args) 
+	{
+		System.out.println("practicing git demo 2 pushing to feature branch ");
+
+	}
+
+}
