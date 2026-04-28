04a09f3898fc2e49790e0a5c8b29c1ddaa286f5d
kiranmore <kiranmore@192.168.0.110>
2026-04-28 15:24:35 +0530

    update in freamwork done

--- AdvanceSelenium2/src/test/java/practiceGit/GitDemo1.java
+++ AdvanceSelenium2/src/test/java/practiceGit/GitDemo1.java
@@ -5,6 +5,8 @@ public class GitDemo1 {
 	public static void main(String[] args) 
 	{
 		System.out.println("practicing Git 11-03-2026");
+		
+		System.out.println("practicing Git 28-04-2026");
 
 	}
 
